1e82a8a920ac5b4b656a54a32653f8bd61af5018
ferkki <ferkki@hotmail.com>
2019-10-18 19:15:40 -0400

    Updates

---
+++ precache-manifest.c98614c2120f6e64c73474afa2a5274e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d766c6e9fe5c96f6149c635b9e755ea",
+    "url": "/frases-breaking-bad/index.html"
+  },
+  {
+    "revision": "32cf32405ce20d03c217",
+    "url": "/frases-breaking-bad/static/css/main.13734ac7.chunk.css"
+  },
+  {
+    "revision": "0378ec790eff0f5fcbf5",
+    "url": "/frases-breaking-bad/static/js/2.2d65e720.chunk.js"
+  },
+  {
+    "revision": "32cf32405ce20d03c217",
+    "url": "/frases-breaking-bad/static/js/main.1ba4454f.chunk.js"
+  },
+  {
+    "revision": "6a90ef1e4c72fadeff69",
+    "url": "/frases-breaking-bad/static/js/runtime-main.883e2dab.js"
+  },
+  {
+    "revision": "427a4adc3c0a0dec192d14617aaa6c4c",
+    "url": "/frases-breaking-bad/static/media/logo.427a4adc.svg"
+  }
+]);
